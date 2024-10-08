a454efed4501e4adab0ee5add75c0ae4c4ca5bde
Ayesha9827 <1.ayesha.1.khan1@gmail.com>
2024-10-08 08:26:55 +0000

    nums_maths

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 100
+// console.log(score);
+
+const count = new Number(100)
+// console.log(count);
+
+// console.log(count.toString().length);
+// console.log(count.toFixed(1));
+
+const num = 23.8934
+
+// console.log(num.toPrecision(3));
+
+
+const value = 5000000
+// console.log(value.toLocaleString('en-IN'))
+
+
+///++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++//
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.3));
+// console.log(Math.ceil(6.9));
+// console.log(Math.floor(3.5))
+// console.log(Math.min(1,2,5,6));
+// console.log(Math.max(4,5,6,9));
+
+console.log(Math.random());
+// console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
