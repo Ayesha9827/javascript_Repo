bc06b9892be186ce8c3bce6076fe917b4e9b5b05
Ayesha9827 <1.ayesha.1.khan1@gmail.com>
2024-10-08 05:47:17 +0000

    Addon

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "ashu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)  
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true ; 0 =>false
 //"" => false
@@ -22,5 +22,45 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//****************************Operations*******************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hii"
+let str2 = " Ashu"
+
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1"+ 2 );
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log(1+ 2 + "2");
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
