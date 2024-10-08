43b9e156e599196f980aef401c1233b4ff8b5d7c
Ayesha9827 <1.ayesha.1.khan1@gmail.com>
2024-10-08 12:17:16 +0000

    object

--- 02_Basics/03_Object.js
+++ 02_Basics/03_Object.js
@@ -1,5 +1,37 @@
-// singleton
+//Singleton
 // Object.create
 
-//object literals
-const JSUser = {}
+// Object literals
+
+const mySym = Symbol("key1")
+
+const JSUser = {
+    name:"Ashu",
+    "full name":"Ayesha Khan",
+    [mySym] : "key1",
+    age : 22,
+    location : "bhopal",
+    email : "abc@gmail.com",
+    isLoggedIn :  false,
+    lastLogininDays : ["Monday","Saturday"]
+}
+// console.log(JSUser.email);
+// console.log(JSUser["email"]);
+// console.log(JSUser["full name"]);
+// console.log(JSUser[mySym]);
+
+JSUser.email = "abc@gmail.com"
+// Object.freeze(JSUser)
+JSUser.email = "abc@yahoo.com"
+// console.log(JSUser);
+
+JSUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JSUser.greetingTwo = function(){
+    console.log(`Hello JS user,${this.name}`);
+}
+
+console.log(JSUser.greeting());
+console.log(JSUser.greetingTwo());
